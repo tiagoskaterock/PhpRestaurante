278701fead1a261128e9913e52aa9af4dacf2ada
tiago <tiagolemespalhano@gmail.com>
2020-11-09 22:31:04 -0300

    Página dos pratos pronta.

--- prato.php
+++ prato.php
@@ -4,29 +4,69 @@
     
   <div class="global-page-container">
 
-    <div class="product-section">
-      <div class="product-info small-12 large-5 columns no-padding">
-        <h3>Camarão ao Alho</h3>
-        <h4>Entradas</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sodales 
-            justo eu mauris tincidunt, id dignissim magna elementum. Sed euismod 
-            efficitur tortor eu facilisis. Proin augue nunc, luctus hendrerit velit 
-            sit amet, iaculis porta velit. In vulputate tristique urna. Praesent 
-            tempus ipsum augue, sit amet tristique lacus semper cursus.     
-        </p>
+    <?php
 
-        <h5><b>Preço: </b>R$ 14</h5>
-        <h5><b>Calorias: </b>310</h5> 
-      </div>
+        $cod_prato = $_GET['prato'];
 
-      <div class="product-picture small-12 large-7 columns no-padding">
-        <img src="img/cardapio/camarao-alho.jpg" alt="camarao">
-      </div>
 
-    </div>
+        $server = 'localhost';
+        $user = 'tiago';
+        $password = '123';
+        $db_name = 'restaurante';
+        $port = '8889';
+
+        $db_connect = new mysqli($server,$user,$password,$db_name,$port);
+        mysqli_set_charset($db_connect,"utf8");
+
+        if ($db_connect->connect_error) {
+            echo 'Falha: ' . $db_connect->connect_error;
+        } 
+        else {
+            $sql = "SELECT * FROM pratos WHERE codigo = '$cod_prato'";
+            $result = $db_connect->query($sql);
+
+            if ($result->num_rows > 0) {
+                
+                while ($row = $result->fetch_assoc()) {
+                    $prato_nome = $row['nome'];
+                    $prato_categoria = $row['categoria'];
+                    $prato_descr = $row['descricao'];
+                    $prato_preco = $row['preco'];
+                    $prato_calorias = $row['calorias'];
+
+                }
+              }
+              else{
+                echo "Não há destaques<br>";
+              }
+          }
+    ?>
+
+    <?php 
+        if ($prato_nome != NULL) { ?>
+            <div class="product-section">
+              <div class="product-info small-12 large-5 columns no-padding">
+                <h3><?php echo $prato_nome; ?></h3>
+                <h4><?php echo $prato_categoria; ?></h4>
+                <p><?php echo $prato_descr; ?>     
+                </p>
+
+                <h5><b>Preço: </b><?php echo $prato_preco; ?></h5>
+                <h5><b>Calorias: </b><?php echo $prato_calorias; ?></h5> 
+              </div>
+
+              <div class="product-picture small-12 large-7 columns no-padding">
+                <img src="img/cardapio/<?php echo $cod_prato; ?>.jpg" alt="Foto do prato: <?php echo $prato_nome; ?>">
+              </div>            
+            <?php
+        } 
+        else{
+            echo "Prato não encontrado<br>";
+        }
+    ?>
 
     <div class="go-back small-12 columns no-padding">
-      <a href="cardapio.html"><< Voltar ao Cardápio</a>
+      <a href="cardapio.php"><< Voltar ao Cardápio</a>
     </div>
 
   </div>
